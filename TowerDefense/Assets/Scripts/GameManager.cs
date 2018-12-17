3150bf5df7ef27437da89e9e94fc09448b3d1b82
promidz <hisyamhanafi@yahoo.com>
2018-12-17 19:02:02 +0800

    TowerDefenseNew Ep27 1.0
    ESKETIT

---
+++ TowerDefense/Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+
+    public static bool gameIsOver;
+
+    public GameObject gameOverUI;
+
+    public string nextLevel = "Level02";
+    public int levelToUnlock = 2;
+
+    public SceneFader sceneFader;
+
+    void Start()
+    {
+        gameIsOver = false;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (gameIsOver)
+            return;
+        
+		if(PlayerStats.Lives <= 0)
+        {
+            EndGame();
+        }
+	}
+    void EndGame()
+    {
+        gameIsOver = true;
+        gameOverUI.SetActive(true);
+        
+    }
+
+    public void WinLevel()
+    {
+        Debug.Log("Level Won!");
+        PlayerPrefs.SetInt("levelReached", levelToUnlock);
+        sceneFader.FadeTo(nextLevel);
+    }
+
+}
---
+++ TowerDefense/Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,59 @@
+﻿//using UnityEngine;
+//using UnityEngine.UI;
+
+
+//public class LevelSelector : MonoBehaviour {
+
+//    public SceneFader fader;
+
+//    public Button[] levelButtons;
+
+//    void Start()
+//    {
+//        int levelReached = PlayerPrefs.GetInt("levelReached",1);
+
+//        for (int i = 0; i < levelButtons.Length; i++)
+//        {
+//            if (i +1 > levelReached)
+//                levelButtons[i].interactable = false;
+
+//        }
+//    }
+
+
+//	public void Select(string levelName)
+//    {
+//        fader.FadeTo(levelName);
+//    }
+//}
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelector : MonoBehaviour
+{
+
+    public SceneFader fader;
+
+    public Button[] levelButtons;
+
+    void Start()
+    {
+        int levelReached = PlayerPrefs.GetInt("levelReached", 1);
+
+        Debug.Log("Level Reached"+levelReached);
+
+        for (int i = 0; i < levelButtons.Length; i++)
+        {
+            if( i +1> levelReached)
+            {
+                levelButtons[i].interactable = false;
+            }
+        }
+    }
+
+    public void Select(string levelName)
+    {
+        fader.FadeTo(levelName);
+    }
+
+}
