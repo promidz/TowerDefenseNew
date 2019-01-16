7cc144ed19edce8008329cc354d20747e7c916b7
hisyamhanafi@yahoo.com <Ibukusayang123>
2019-01-16 20:25:31 +0800

    3.0 Alpha

---
+++ TowerDefense/Assets/Scripts/Bosses.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bosses : MonoBehaviour {
+    [SerializeField]
+    private float timeBetweenChange=2;
+    [SerializeField]
+    private float cooldown=8f;
+    [SerializeField]
+    Text changeCooldown;
+
+    [SerializeField]
+    Image bossImage;
+
+    [SerializeField]
+    Sprite[] bossSprite;
+
+    private void Start()
+    {
+        bossImage.sprite = bossSprite[0];
+        
+    }
+
+    private void Update()
+    {
+        cooldown -= Time.deltaTime;
+        cooldown = Mathf.Clamp(cooldown, 0f, Mathf.Infinity);
+        changeCooldown.text = "DANGER: "+string.Format("{0:00.00}", cooldown);
+
+        if(cooldown <= 0)
+        {
+            ChangeImage();
+
+        }
+    }
+    public void ChangeImage() {
+
+        bossImage.sprite = bossSprite[1];
+
+    }
+    IEnumerator 
+
+}
